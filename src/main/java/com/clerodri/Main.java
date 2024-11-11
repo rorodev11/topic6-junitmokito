4d0d7674d17368bb1167e20986aa5387b3e5b07c
Ronaldo Rodriguez <rrodriguez@kvz.cl>
2024-11-11 18:51:27 -0500

    first commita

--- src/main/java/com/clerodri/Main.java
+++ src/main/java/com/clerodri/Main.java
@@ -2,6 +2,6 @@ package com.clerodri;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wSDFGorld!");
+        System.out.println("Hello wSDFGorlaaad!");
     }
 }
